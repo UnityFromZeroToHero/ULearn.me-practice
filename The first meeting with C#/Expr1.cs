f381dbe5a953ba201b7d724609f476b0d9ff1718
Dmitry <78854763+Doggsterio@users.noreply.github.com>
2022-12-02 01:48:41 +0300

    Create Expr1.cs

---
+++ The first meeting with C#/Expr1.cs
@@ -0,0 +1,25 @@
+//Expr1. Как поменять местами значения двух переменных?
+//Можно ли это сделать без ещё одной временной переменной?
+//Стоит ли так делать?
+
+using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            int firstVariable = 2;
+            int secondVariable = 5;
+
+            //Exchange of values of two variables by using the 3rd one.
+            /*int thirdVariable = firstVariable;
+            firstVariable = secondVariable;
+            secondVariable = thirdVariable;*/
+            
+            //Exchange of values of two variables without using the 3rd one.
+            (firstVariable, secondVariable) = (secondVariable, firstVariable);
+        }
+    }
+}
