543eceaf2f85b6515d5937d2bac892ae7c2d2f3c
Dmitry <78854763+Doggsterio@users.noreply.github.com>
2022-12-20 21:07:03 +0300

    Update 3 - Billiard.cs

--- Basics of programming/2 - Errors/Exercises/3 - Billiard.cs
+++ Basics of programming/2 - Errors/Exercises/3 - Billiard.cs
@@ -1,1 +1,22 @@
-?
+//Реализуйте метод для расчета угла отскока шарика от стены. 
+//Считайте, что угол падения равен углу отражения, то есть можно пренебречь всеми физическими эффектами, связанными с кручением шаров, трением шара об стенку и т.п.
+
+using System;
+
+namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directionRadians">Угол направления движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns></returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            //TODO
+            return 2*wallInclinationRadians - directionRadians;
+        }
+    }
+}
