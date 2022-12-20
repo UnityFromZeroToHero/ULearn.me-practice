67930f52492a5e47e9c2d9f2b066acbd17a97760
Dmitry <78854763+Doggsterio@users.noreply.github.com>
2022-12-20 20:30:05 +0300

    Update 6 - Distance to the line segment.cs

--- Basics of programming/3 - Branching/Exercises/6 - Distance to the line segment.cs
+++ Basics of programming/3 - Branching/Exercises/6 - Distance to the line segment.cs
@@ -1,1 +1,34 @@
-?
+//Даны координаты точки и координаты концов отрезка.
+//Нужно найти расстояние от этой точки до отрезка.
+
+using System;
+
+namespace DistanceTask
+{
+	public static class DistanceTask
+	{
+		public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+		{
+			double eps = 1e-8;
+
+			double lengthAB = Math.Sqrt((bx - ax)*(bx - ax) + (by - ay)*(by - ay));
+			double lengthAC = Math.Sqrt((x - ax)*(x - ax) + (y - ay)*(y - ay));
+			double lengthBC = Math.Sqrt((x - bx)*(x - bx) + (y - by)*(y - by));
+
+			if (lengthAB != 0)
+			{
+				if (lengthAC*lengthAC + lengthAB*lengthAB >= lengthBC*lengthBC &&
+					lengthAB*lengthAB + lengthBC*lengthBC >= lengthAC*lengthAC)
+				{
+					return Math.Abs((by - ay) * x - (bx - ax) * y + bx * ay - by * ax) /
+						Math.Sqrt((by - ay)*(by - ay) + (bx - ax)*(bx - ax));
+				}
+				else if (Math.Abs(lengthAC + lengthBC - lengthAB) < eps)
+				{
+					return 0.0;
+				}
+			}	
+			return Math.Min(lengthAC, lengthBC);
+		}
+    }
+}
