df0bca5b30b644a7e8fbda163330d9fcb2c4b145
Dmitry <78854763+Doggsterio@users.noreply.github.com>
2022-12-20 03:00:47 +0300

    Update 1 - Leap year.cs

---
+++ Basics of programming/3 - Branching/Exercises/1 - Leap year.cs
@@ -0,0 +1,6 @@
+//Функция определяет високосный год или нет.
+
+public static bool IsLeapYear(int year)
+{
+    return year % 400 == 0 ? true : (year % 100 == 0 ? false : (year % 4 == 0 ? true : false));
+}
